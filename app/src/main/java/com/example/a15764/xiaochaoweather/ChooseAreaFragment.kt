bfa1345397cee064625ba71ce766c4f79ed59e57
小朝 <13942156310@163.com>
2017-05-28 18:43:18 +0800

    stage1 ok

--- app/src/main/java/com/example/a15764/xiaochaoweather/ChooseAreaFragment.kt
+++ app/src/main/java/com/example/a15764/xiaochaoweather/ChooseAreaFragment.kt
@@ -1,47 +1,213 @@
 package com.example.a15764.xiaochaoweather
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 import com.example.a15764.xiaochaoweather.db.City
 import com.example.a15764.xiaochaoweather.db.County
 import com.example.a15764.xiaochaoweather.db.Province
+import com.example.a15764.xiaochaoweather.util.handleCityReponse
+import com.example.a15764.xiaochaoweather.util.handleCountyReponse
+import com.example.a15764.xiaochaoweather.util.handleProvinceReponse
+import com.example.a15764.xiaochaoweather.util.sendOkHttpRequest
 import kotlinx.android.synthetic.main.fragment_choose_area.*
-
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import org.jetbrains.anko.find
+import org.jetbrains.anko.support.v4.onUiThread
+import org.jetbrains.anko.support.v4.toast
+import org.litepal.crud.DataSupport
+import java.io.IOException
 
 class ChooseAreaFragment : Fragment() {
 
-    // TODO: Rename and change types of parameters
     val LEVEL_PROVINCE = 0
     val LEVEL_CITY = 1
     val LEVEL_COUNTRY = 2
-    private var adapter: ArrayAdapter<String>?=null
+
+    private val TAG = "ChooseAreaFragment"
+
+    private var queryProgressDialog: ProgressDialog? = null
+    private var titleText: TextView?= null
+    private var backButtton: Button?= null
+    private var listView: ListView?= null
+    private var adapter: ArrayAdapter<String>? = null
     private var datalist = ArrayList<String>()
     //省列表
-    private var provinceList: List<Province>?=null
+    private var provinceList: MutableList<Province>? = null
     //市列表
-    private var cityList: List<City>?=null
+    private var cityList: MutableList<City>? = null
     //县列表
-    private var countryList: List<County>?=null
+    private var countryList: MutableList<County>? = null
     //选中的省份
-    private var selectedProvince: Province?=null
+    private var selectedProvince: Province? = null
     //选中的城市
-    private var selectedCity: City?=null
+    private var selectedCity: City? = null
     //当前选中的级别
-    private var currentLevel: Int?=null
+    private var currentLevel: Int? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_choose_area, container, false)
+        titleText = view.find(R.id.title_text)
+        backButtton = view.find(R.id.back_button)
+        listView = view.findViewById(R.id.list_view) as ListView
+
         adapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, datalist)
-        list_view.adapter = adapter
+        listView?.adapter = adapter
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-  //      list_view.setOnItemClickListener { parent, view, position, id ->  }
+        listView?.setOnItemClickListener { parent, view, position, id ->
+            if (currentLevel == LEVEL_PROVINCE) {
+                selectedProvince = provinceList!!.get(position)
+                queryCities()
+            }
+            else if (currentLevel == LEVEL_CITY){
+                selectedCity = cityList!!.get(position)
+                queryCounties()
+            }
+        }
+        backButtton?.setOnClickListener {
+            when(currentLevel){
+                LEVEL_COUNTRY -> queryCities()
+                LEVEL_CITY -> queryProvinces()
+            }
+        }
+        queryProvinces()
+    }
+
+    /**
+     * 查询全国所有的省，优先从数据库查询，如果没有再去服务器查询
+     */
+    private fun queryProvinces() {
+        titleText?.text = "中国"
+        backButtton?.visibility = View.GONE
+        provinceList = DataSupport.findAll(Province::class.java)
+        if (provinceList!!.size > 0) {
+            datalist.clear()
+            provinceList?.forEach {
+                datalist.add(it.provinceName)
+            }
+            adapter?.notifyDataSetChanged()
+            list_view.setSelection(0)
+            currentLevel = LEVEL_PROVINCE
+        } else {
+            val address = "http://guolin.tech/api/china"
+            queryFromServer(address, "province")
+        }
+
+    }
+
+
+    private fun queryCities(){
+        titleText?.text = selectedProvince!!.provinceName
+        backButtton?.visibility = View.VISIBLE
+        cityList = DataSupport.where("provinceid = ?", "${selectedProvince?.id}").find(City::class.java)
+        if (cityList!!.size>0){
+            datalist.clear()
+            cityList?.forEach {
+                datalist.add(it.cityName)
+            }
+            adapter?.notifyDataSetChanged()
+            list_view.setSelection(0)
+            currentLevel = LEVEL_CITY
+        }
+        else{
+            val provinceCode = selectedProvince!!.provinceCode
+            log(provinceCode.toString())
+            val address = "http://guolin.tech/api/china/$provinceCode"
+            queryFromServer(address, "city")
+        }
+    }
+
+
+    private fun queryCounties(){
+        titleText?.text = selectedCity?.cityName
+        backButtton?.visibility = View.VISIBLE
+        countryList = DataSupport.where("cityid = ?", "${selectedCity?.id}").find(County::class.java)
+        if(countryList!!.size>0){
+            datalist.clear()
+            countryList?.forEach {
+                datalist.add(it.countyName)
+            }
+            adapter?.notifyDataSetChanged()
+            list_view.setSelection(0)
+            currentLevel = LEVEL_COUNTRY
+        }
+        else{
+            val provenceCode = selectedProvince?.provinceCode
+            val cityCode = selectedCity?.cityCode
+            val address = "http://guolin.tech/api/china/$provenceCode/$cityCode"
+            log("$provenceCode and $cityCode")
+            queryFromServer(address, "country")
+        }
+
+    }
+
+
+    private fun queryFromServer(address: String, type: String) {
+        showProcessDialog()
+        sendOkHttpRequest(address, object :Callback{
+            override fun onResponse(call: Call?, response: Response?) {
+                val responseText = response!!.body().string()
+                log(responseText)
+                log(type)
+                var result = false
+                when(type) {
+                    "province"-> result = handleProvinceReponse(responseText)
+                    "city" -> result = handleCityReponse(responseText, selectedProvince!!.id)
+                    "country" -> result = handleCountyReponse(responseText, selectedCity!!.id)
+                }
+                if (result){
+                    onUiThread {
+                        closeProgressDialog()
+                        when(type){
+                            "province" -> queryProvinces()
+                            "city" -> queryCities()
+                            "country" -> queryCounties()
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                onUiThread {
+                    closeProgressDialog()
+                    toast("加载失败")
+                }
+            }
+        })
+    }
+
+
+    private fun showProcessDialog() {
+        if (queryProgressDialog == null){
+            queryProgressDialog = ProgressDialog(activity)
+        }
+        queryProgressDialog?.setMessage("loading...")
+        queryProgressDialog?.setCanceledOnTouchOutside(false)
+        queryProgressDialog?.show()
+
+    }
+
+    private fun closeProgressDialog(){
+        queryProgressDialog?.dismiss()
+    }
+
+    private fun log(message:String){
+        Log.d(TAG, message)
     }
 }
--- app/src/main/java/com/example/a15764/xiaochaoweather/util/utils.kt
+++ app/src/main/java/com/example/a15764/xiaochaoweather/util/utils.kt
@@ -1,11 +1,13 @@
 package com.example.a15764.xiaochaoweather.util
 
 import android.text.TextUtils
+import android.util.Log
 import com.example.a15764.xiaochaoweather.db.City
 import com.example.a15764.xiaochaoweather.db.County
 import com.example.a15764.xiaochaoweather.db.Province
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jetbrains.anko.AnkoLogger
 import org.json.JSONArray
 import org.json.JSONException
 
@@ -13,7 +15,7 @@ import org.json.JSONException
 /**
  * 与服务器进行交互，发送http请求，注册一个回调来处理响应
  */
-public fun sendOkHttpRequest(address: String,callback:okhttp3.Callback){
+fun sendOkHttpRequest(address: String,callback:okhttp3.Callback){
     val client = okhttp3.OkHttpClient()
     val request = okhttp3.Request.Builder().url(address).build()
     client.newCall(request).enqueue(callback)
@@ -22,11 +24,12 @@ public fun sendOkHttpRequest(address: String,callback:okhttp3.Callback){
 /**
  * 解析和处理服务器返回的省级数据
  */
-public fun handleProvinceReponse(response: String):Boolean{
+fun handleProvinceReponse(response: String):Boolean{
     if (!android.text.TextUtils.isEmpty(response)){
         try {
             val allProvinces = org.json.JSONArray(response)
-            for (i in 0..allProvinces.length()){
+            Log.d("utils","pro:${allProvinces.length()}")
+            for (i in 0..allProvinces.length()-1){
                 val provenceObject = allProvinces.getJSONObject(i)
                 val province = com.example.a15764.xiaochaoweather.db.Province()
                 province.provinceName = provenceObject.getString("name")
@@ -45,11 +48,12 @@ public fun handleProvinceReponse(response: String):Boolean{
 /**
  * 解析和处理服务器返回的市级数据
  */
-public fun handleCityReponse(response: String, provinceId: Int):Boolean{
+fun handleCityReponse(response: String, provinceId: Int):Boolean{
     if (!android.text.TextUtils.isEmpty(response)){
         try {
             val allCitys = org.json.JSONArray(response)
-            for (i in 0..allCitys.length()){
+            Log.d("utils","city:${allCitys.length()}")
+            for (i in 0..allCitys.length()-1){
                 val cityObject = allCitys.getJSONObject(i)
                 val city = com.example.a15764.xiaochaoweather.db.City()
                 city.cityName = cityObject.getString("name")
@@ -69,11 +73,12 @@ public fun handleCityReponse(response: String, provinceId: Int):Boolean{
 /**
  * 解析和处理服务器返回的县级数据
  */
-public fun handleCountyReponse(response: String, cityId: Int):Boolean{
+fun handleCountyReponse(response: String, cityId: Int):Boolean{
     if (!android.text.TextUtils.isEmpty(response)){
         try {
             val allCounties = org.json.JSONArray(response)
-            for (i in 0..allCounties.length()){
+            Log.d("utils","country:${allCounties.length()}")
+            for (i in 0..allCounties.length()-1){
                 val countryObject = allCounties.getJSONObject(i)
                 val country = com.example.a15764.xiaochaoweather.db.County()
                 country.countyName = countryObject.getString("name")
