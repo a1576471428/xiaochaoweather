7febc1bb2f112a779cd1bbfb98447d1330b803f3
小朝 <13942156310@163.com>
2017-05-30 13:29:26 +0800

    背景图片ok

--- app/src/main/java/com/example/a15764/xiaochaoweather/activitys/WeatherActivity.kt
+++ app/src/main/java/com/example/a15764/xiaochaoweather/activitys/WeatherActivity.kt
@@ -1,11 +1,14 @@
 package com.example.a15764.xiaochaoweather.activitys
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 import com.example.a15764.xiaochaoweather.R
 import com.example.a15764.xiaochaoweather.gson.WeatherData
@@ -26,9 +29,18 @@ import java.io.IOException
 
 class WeatherActivity : AppCompatActivity() {
     val key = "6379fa59b03042e1b75d4c3e27da69c0"
+    val backgrounPicApiUrl = "http://guolin.tech/api/bing_pic"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= 21){
+            val decorView = window.decorView
+            // 这俩表示活动的布局会显示在状态栏上面
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
         setContentView(R.layout.activity_weather)
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
@@ -41,6 +53,13 @@ class WeatherActivity : AppCompatActivity() {
             val cityId = intent.getStringExtra("city_id")
             weather_layout.visibility = View.INVISIBLE
             requestWeather(cityId)
+        }
+        val backgrounPic = prefs.getString("background_pic", null)
+        if (backgrounPic != null){
+            Glide.with(this@WeatherActivity).load(backgrounPic).into(background_pic)
+        }
+        else{
+            loadBackgroundPic()
         }
     }
 
@@ -77,6 +96,7 @@ class WeatherActivity : AppCompatActivity() {
                 }
             }
         })
+        loadBackgroundPic()
     }
 
     /**
@@ -120,6 +140,24 @@ class WeatherActivity : AppCompatActivity() {
         car_wash_text.text = "洗车指数：${weather?.heweather5?.get(0)?.suggestion?.cw?.txt}"
         sport_text.text = "运动建议：${weather?.heweather5?.get(0)?.suggestion?.sport?.txt}"
         weather_layout.visibility = View.VISIBLE
+    }
+
+    private fun loadBackgroundPic(){
+        sendOkHttpRequest(backgrounPicApiUrl, object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                e?.printStackTrace()
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val backgroundPic = response?.body()?.string()
+                val editor = PreferenceManager.getDefaultSharedPreferences(this@WeatherActivity).edit()
+                editor.putString("background_pic", backgroundPic)
+                editor.apply()
+                runOnUiThread {
+                    Glide.with(this@WeatherActivity).load(backgroundPic).into(background_pic)
+                }
+            }
+        })
     }
 
 
